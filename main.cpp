c39e4b437938499a7ba4bc7401898006dc91fb7b
Dereo <91685915+Heldmin@users.noreply.github.com>
2022-03-25 19:27:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@ using namspace std;
 
 int main(){
 	
-double a,b;
+double a,b, c, b;
 
 cout << "Input your first number" << endl;	
 cin >> a;	
 cout << "Input your second number" << endl;	
 cin >> b;
+cout << "Input your 3rd number" << endl;	
+cin >> a;	
+cout << "Input your 4th number" << endl;	
+cin >> b;	
 	
-cout << "There sum equals: " << a+b << endl;	
+cout << "Their sum equals: " << a+b+c+d << endl;	
 return 0;
 }
