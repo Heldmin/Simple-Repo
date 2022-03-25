b0d5835dd6fbd5fa2c2e59a73dd4de10fcbb47eb
Dereo <91685915+Heldmin@users.noreply.github.com>
2022-03-25 19:37:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,27 @@ using namspace std;
 
 int main(){
 	
-double a,b;
-
+double a, b, c, d;
+	
 cout << "Input your first number" << endl;	
 cin >> a;	
 cout << "Input your second number" << endl;	
 cin >> b;
+cout << "Input your 3rd number" << endl;	
+cin >> c;	
+cout << "Input your 4th number" << endl;	
+cin >> c;
+
+double max;	
 	
-cout << "There sum equals: " << a+b << endl;	
+if(a > b && a > c && a > d){
+max = a;	
+}else if(b > d && b > c){
+max = b;
+}else if(c > d){
+max = c	;
+}
+	
+cout << "The biggest number is: " << max << endl;	
 return 0;
 }
